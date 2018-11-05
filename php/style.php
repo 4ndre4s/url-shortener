cfcba053561779036607386ffb2191224e49b0b3
Hugoz22 <andreas.partsch@informatik.hs-fulda.de>
2018-11-05 23:53:48 +0100

    moved style to subdomain

--- php/style.php
+++ php/style.php
@@ -1,3 +1,3 @@
-<link rel="stylesheet" type="text/css" href="../style/footer.css">
-<link rel="stylesheet" href="../style/basic.css">
-<link rel="stylesheet" href="../style/form.css">
+<link rel="stylesheet" type="text/css" href="http://style.make-short.xyz/footer.css">
+<link rel="stylesheet" href="http://style.make-short.xyz/style/basic.css">
+<link rel="stylesheet" href="http://style.make-short.xyz/form.css">
