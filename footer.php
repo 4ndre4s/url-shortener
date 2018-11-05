f89d2152944439ef5520ec0b8bbac197d0da9b1b
Hugoz22 <andreas.partsch@informatik.hs-fulda.de>
2018-11-05 22:11:28 +0100

    fixed style

--- footer.php
+++ footer.php
@@ -1,9 +1,7 @@
 <div id="footer">
-    <div id="footer">
-        <a href="https://github.com/Hugoz22/url-shortener">
-            <div class="footer_element">
-                Code
-            </div>
-        </a>
-    </div>
+    <a href="https://github.com/Hugoz22/url-shortener">
+        <div class="footer_element">
+            Code
+        </div>
+    </a>
 </div>
