761993dae9c1bab7e41e54262a946d2608905da5
Hugoz22 <andreas.partsch@informatik.hs-fulda.de>
2018-11-05 22:02:43 +0100

    added styling

---
+++ footer.php
@@ -0,0 +1,9 @@
+<div id="footer">
+    <div id="footer">
+        <a href="https://github.com/Hugoz22/url-shortener">
+            <div class="footer_element">
+                Code
+            </div>
+        </a>
+    </div>
+</div>
---
+++ style.php
@@ -0,0 +1,3 @@
+<link rel="stylesheet" type="text/css" href="./style/footer.css">
+<link rel="stylesheet" href="./style/basic.css">
+<link rel="stylesheet" href="./style/form.css">
