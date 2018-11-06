478184a8329c1b9769f10053b73395aec53f306d
Hugoz22 <andreas.partsch@informatik.hs-fulda.de>
2018-11-06 18:57:35 +0100

    changed http to https

--- php/style.php
+++ php/style.php
@@ -1,3 +1,3 @@
-<link rel="stylesheet" type="text/css" href="http://style.make-short.xyz/footer.css">
-<link rel="stylesheet" href="http://style.make-short.xyz/style/basic.css">
-<link rel="stylesheet" href="http://style.make-short.xyz/form.css">
+<link rel="stylesheet" type="text/css" href="https://style.make-short.xyz/footer.css">
+<link rel="stylesheet" href="https://style.make-short.xyz/style/basic.css">
+<link rel="stylesheet" href="https://style.make-short.xyz/form.css">
