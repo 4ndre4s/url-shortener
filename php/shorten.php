3142e65af188cdb0b967c5d2dd1cec1f985c96fe
andi <andreas.partsch@informatik.hs-fulda.de>
2018-11-08 10:57:17 +0100

    title changes dynamically

--- php/shorten.php
+++ php/shorten.php
@@ -1,32 +1,31 @@
+<?php
+define("token_length", 7);
+$token = file_get_contents("http://random-token.xyz/". token_length, False);
+$url = htmlspecialchars($_POST['url']);
+$domain = "https://view.make-short.xyz";
+
+//create URL-Textfile
+if (filter_var($url, FILTER_VALIDATE_URL)) {
+    $isUrl = true;
+    file_put_contents("../shorted/" . $token . ".txt", $url);
+    //create .php-File from default.php
+    copy("default.php", "../shorted/" . $token . ".php");
+    $link = $domain . "/" . $token . ".php";
+
+} else {
+    $isUrl = false;
+}
+
+?>
+
 <!doctype html>
 <html lang="en">
 <head>
     <meta charset="UTF-8">
     <?php include 'style.php' ?>
-    <title>Url has been shortened</title>
+    <title><?php echo ($isUrl ? "Url has been shortened!" : "Error!"); ?> </title>
 </head>
 <body>
-    <?php
-        define("token_length", 7);
-        $token = file_get_contents("http://random-token.xyz/". token_length, False);
-        $url = htmlspecialchars($_POST['url']);
-        $domain = "https://view.make-short.xyz";
-
-
-        //TODO: check, if it is a url
-        //create URL-Textfile
-        if (filter_var($url, FILTER_VALIDATE_URL)) {
-            $isUrl = true;
-            file_put_contents("../shorted/" . $token . ".txt", $url);
-            //create .php-File from default.php
-            copy("default.php", "../shorted/" . $token . ".php");
-            $link = $domain . "/" . $token . ".php";
-
-        } else {
-            $isUrl = false;
-        }
-
-        ?>
     <div class="outer_container">
         <div class="inner_container">
             <?php
@@ -38,7 +37,6 @@
             ?>
         </div>
     </div>
-
     <?php
         include 'footer.php';
     ?>
